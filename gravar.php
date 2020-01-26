2b1cdb859190541ad9b414eba3123e44dad86b49
Biangelica <43317376+Biangelica@users.noreply.github.com>
2020-01-26 12:42:41 -0300

    Update gravar.php
    adicionando a gravação do id da cidade a tabela contatos

--- gravar.php
+++ gravar.php
@@ -4,8 +4,9 @@
     $nome=$_GET["nome"];
     $email=$_GET["email"];
     $tel=$_GET["phone"];
+    $cidade=$_GET["city"];
     
-      mysqli_query(abrirBanco(), "insert into contato (nome, email, telefone) values ('$nome', '$email', '$tel'");
+      mysqli_query(abrirBanco(), "insert into contato (nome, email, telefone, cidade) values ('$nome', '$email', '$tel', '$cidade'");
     
     header("location:list.php");
  ?>
