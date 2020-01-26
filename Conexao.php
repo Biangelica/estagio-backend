ed068d09f093608262c1494119beb4344f73edff
Biangelica <43317376+Biangelica@users.noreply.github.com>
2020-01-26 12:59:23 -0300

    Update Conexao.php
    adicionando a função que seleciona todos os dados do banco

--- Conexao.php
+++ Conexao.php
@@ -5,3 +5,14 @@ if(!function_exists('abrirBanco')) {
     return $conexao;
     }
 }
+if(!function_exists('selectAllData')) {
+    function selectAllData() {
+     $banco = abrirBanco();
+    $sql = "select con.*, cid.nomeCidade, cid.uf  from contato con inner join cidades cid on con.cidade = cid.id order by nomeCidade ";
+    $resultado = $banco->query($sql);
+    while($row = mysqli_fetch_array($resultado)){
+        $all[] = $row;
+    }
+    return $all;
+    }
+}
